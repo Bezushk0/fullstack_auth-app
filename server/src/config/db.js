30c302a848c0bd98ab2073b61d867f1f7b41631f
Vladislav Bezushko <bezushkovladis@gmail.com>
2025-03-28 14:52:48 +0200

    add ssl options 1

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,18 +2,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?sslmode=require`,
   {
-    host: process.env.DB_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
     logging: false,
   },
 );
