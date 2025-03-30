c7ba41e544c394e6883b139e90fcc9c0af9bd29c
Vladislav Bezushko <bezushkovladis@gmail.com>
2025-03-30 17:01:56 +0300

    Updated cors

--- server/src/index.js
+++ server/src/index.js
@@ -12,17 +12,17 @@ const { authRouter } = require('./routes/authRoutes');
 const { userRouter } = require('./routes/userRoutes');
 const { errorMiddleware } = require('./middlewares/errorMiddleware');
 
-const PORT = process.env.PORT || 5000;
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_HOST,
-    credentials: true,
-  }),
-);
+const corsOptions = {
+  origin: process.env.CLIENT_HOST,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(errorMiddleware);
 
 app.use(authRouter);
 app.use('/users', userRouter);
@@ -31,6 +31,6 @@ app.use((req, res) => {
   res.status(404).send('The page is not found');
 });
 
-app.use(errorMiddleware);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
